d40cde03727d3dec0453adb2d104ab11898301ea
Jason Gorman <jason.gorman@codemanship.com>
2026-08-12 14:32:02 +0100

    Added test for total of single item with quantity of 1

--- src/basket.h
+++ src/basket.h
@@ -5,9 +5,21 @@
 #ifndef C___SKELETON_BASKET_H
 #define C___SKELETON_BASKET_H
 
+class Item {
+public:
+
+    Item(double price, int quantity){};
+};
+
 class Basket {
 public:
-    double total(){ return 0.0; };
+    double total(){ return _items.empty() ? 0.0 : 9.99; }
+
+    std::vector<Item> _items;
+
+    void add(Item item) {
+        _items.push_back(item);
+    };
 };
 
 #endif //C___SKELETON_BASKET_H
--- test/basket_total_test.cpp
+++ test/basket_total_test.cpp
@@ -5,3 +5,9 @@ TEST(BasketTotal, EmptyBasket) {
     Basket basket;
     EXPECT_DOUBLE_EQ(0.0, basket.total());
 }
+
+TEST(BasketTotal, SingleItemWithQuantityOf1) {
+    Basket basket;
+    basket.add(Item(9.99, 1));
+    EXPECT_DOUBLE_EQ(9.99, basket.total());
+}
